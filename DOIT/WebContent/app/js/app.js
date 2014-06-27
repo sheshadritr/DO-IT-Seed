6f3b5ea160d21a2414b9031399a61a9c34d4fa21
Sheshadri <sheshadri.tr@hashedin.com>
2014-06-27 17:30:33 +0530

    added transition from view1 to view2, a basic pop up in view1 to create project, controller updated for both views

--- DOIT/WebContent/app/js/app.js
+++ DOIT/WebContent/app/js/app.js
@@ -14,15 +14,39 @@ config(['$routeProvider', '$locationProvider', function($routeProvider, $locatio
   $locationProvider.html5Mode(true);
   $routeProvider.
   when('/view1', {
-	  templateUrl: 'partials/view1.html',
+	  templateUrl: '/partials/view1.html',
 	  controller: 'ProjectListController'}).
   when('/view1/:projectId', {
-	  templateUrl: 'partials/view2.html',
+	  templateUrl: '/partials/view2.html',
 	  controller: 'ProjectDetailsController'}).
   otherwise({
       redirectTo: '/view1'
     });
 }]);
+
+
+var ModalDemoCtrl = function ($scope, $modal, $log) {
+	  $scope.items = ['item1', 'item2', 'item3'];
+	  $scope.open = function (size) {
+	    var modalInstance = $modal.open({
+	      templateUrl: 'myModalContent.html',
+	      controller: ModalInstanceCtrl,
+	      size: size,
+	      resolve: {
+	        items: function () {
+	          return $scope.items;
+	        }
+	      }
+	    });
+
+	    modalInstance.result.then(function (selectedItem) {
+	      $scope.selected = selectedItem;
+	    }, function () {
+	      $log.info('Modal dismissed at: ' + new Date());
+	    });
+	  };
+	};
+
 
 angular.module('myApp').constant('appSettings', {
   AppURI: 'http://127.0.0.1:9080/'
--- DOIT/WebContent/app/js/controllers.js
+++ DOIT/WebContent/app/js/controllers.js
@@ -4,52 +4,72 @@
 
 angular.module('myApp.controllers', [])
   .controller('ProjectListController', ['$scope', function($scope) {
-	  $scope.projects = [
-	                     {
-	                    	 "projectId": "1",
+	  $scope.projects = [{
+	                    	 "projectId": 1,
 	                    	 "name": "angular js",
 	                    	 "description": "assignment 3",
-	                    	 "coordinators": [ "Chandan",
-	                    	                   "Ralf",
-	                    	                   "Anurag"
-	                    	                  ]
-	                     },
-	                     {
-	                    	 "projectId": "2",
+	                    	 "coordinators": [ "Chandan", "Ralf", "Anurag" ]
+	                      },
+	                      {
+	                    	 "projectId": 2,
 	                    	 "name": "java script",
 	                    	 "description": "assignment 2",
-		                    	 "coordinators": [ "Vinod",
-		                    	                   "Himanshu",
-		                    	                   "Manish"
-		                    	                  ]
-	                     },
-	                     {
-	                    	 "projectId": "3",
+		                    	 "coordinators": [ "Vinod", "Himanshu", "Manish" ]
+	                      },
+	                      {
+	                    	 "projectId": 3,
 	                    	 "name": "pixel perfect",
 	                    	 "description": "assignment 1",
-	                    	 "coordinators": [ "Sri",
-	                    	                   "Nagarjun",
-	                    	                   "Vinith"
-	                    	                  ]
-	                     }
-	                     ]
+	                    	 "coordinators": [ "Sri", "Nagarjun", "Vinith" ]
+	                     }];
   }])
-  .controller('ProjectDetailsController', ['$scope', function($scope) {
-    $scope.projectdetails = [
-                             {
-                            	 "projectId": "1",
-                            	 "tasks": [
-                            	           	{
+  .controller('ProjectDetailsController', ['$scope', '$routeParams',
+                                           function($scope, $routeParams) {
+    $scope.projectDetails = [{
+                            	 "projectId": 1,
+                              	"coordinators": [{
+                              						"name": "Ralf", "role": "Developer"
+                              					   },
+                              					   {
+                              						 "name": "Chandan", "role": "Developer"
+                              					   }],
+                            	 "tasks": [{
                             	        	   "title": "Create basic scaffold",
                             	        	   "status": "pending",
-                            	        	   "info": "Create a basic project structure for HU-Web"
+                            	        	   "info": "Create a basic project structure for HU-Web",
+                            	        	   "deuDate": "27-06-2014", "createdOn": "26-06-2014", "closedOn": "27-06-2014",
+                            	        	   "assignedTo": "Chandan"
                             	           	},
                             	           	{
                             	        	   "title": "Ensure responsive",
                             	        	   "status": "pending",
-                            	        	   "info": "Create css to ensure responsiveness of site"
-                            	           	}
-                            	           ]
-                             }
-                             ]
+                            	        	   "info": "Create css to ensure responsiveness of site",
+                            	        	   "deuDate": "27-06-2014", "createdOn": "26-06-2014", "closedOn": "27-06-2014",
+                            	        	   "assignedTo": "Ralf"
+                            	           	}]
+                             }];
+    
+    for(var i=0; i < $scope.projectDetails.length; i++) {
+    	if($routeParams.projectId == $scope.projectDetails.id) {
+    		$scope.projectInfo = $scope.projectDetails;
+    	}
+    }
+    
   }]);
+
+
+var ModalInstanceCtrl = function ($scope, $modalInstance, items) {
+
+	  $scope.items = items;
+	  $scope.selected = {
+	    item: $scope.items[0]
+	  };
+
+	  $scope.ok = function () {
+	    $modalInstance.close($scope.selected.item);
+	  };
+
+	  $scope.cancel = function () {
+	    $modalInstance.dismiss('cancel');
+	  };
+	};

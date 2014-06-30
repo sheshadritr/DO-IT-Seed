238ab4686edaffd446747670515e847908748d02
Sheshadri <sheshadri.tr@hashedin.com>
2014-06-30 10:35:18 +0530

    complete table in view2, partial filter of view2

--- DOIT/WebContent/app/js/controllers.js
+++ DOIT/WebContent/app/js/controllers.js
@@ -7,31 +7,34 @@ angular.module('myApp.controllers', [])
 	  $scope.projects = [{
 	                    	 "projectId": 1,
 	                    	 "name": "angular js",
-	                    	 "description": "assignment 3",
-	                    	 "coordinators": [ "Chandan", "Ralf", "Anurag" ]
+	                    	 "description": "assignment 1",
+	                    	 "collaborators": [ "Chandan", "Ralf", "Anurag" ]
 	                      },
 	                      {
 	                    	 "projectId": 2,
 	                    	 "name": "java script",
 	                    	 "description": "assignment 2",
-		                    	 "coordinators": [ "Vinod", "Himanshu", "Manish" ]
+		                     "collaborators": [ "Vinod", "Himanshu", "Manish" ]
 	                      },
 	                      {
 	                    	 "projectId": 3,
 	                    	 "name": "pixel perfect",
-	                    	 "description": "assignment 1",
-	                    	 "coordinators": [ "Sri", "Nagarjun", "Vinith" ]
+	                    	 "description": "assignment 3",
+	                    	 "collaborators": [ "Sri", "Nagarjun", "Vinith" ]
 	                     }];
   }])
   .controller('ProjectDetailsController', ['$scope', '$routeParams',
                                            function($scope, $routeParams) {
     $scope.projectDetails = [{
                             	 "projectId": 1,
-                              	"coordinators": [{
+                              	"collaborators": [{
                               						"name": "Ralf", "role": "Developer"
                               					   },
                               					   {
                               						 "name": "Chandan", "role": "Developer"
+                              					   },
+                              					   {
+                              						  "name": "Anurag", "role": "Developer"
                               					   }],
                             	 "tasks": [{
                             	        	   "title": "Create basic scaffold",
@@ -47,14 +50,38 @@ angular.module('myApp.controllers', [])
                             	        	   "deuDate": "27-06-2014", "createdOn": "26-06-2014", "closedOn": "27-06-2014",
                             	        	   "assignedTo": "Ralf"
                             	           	}]
+                             },
+                             {
+                            	 "projectId": 2,
+                              	"collaborators": [{
+                              						"name": "Vinod", "role": "Developer"
+                              					   },
+                              					   {
+                              						 "name": "Himanshu", "role": "Developer"
+                              					   },
+                              					   {
+                                						 "name": "Manish", "role": "Developer"
+                                				   }],
+                            	 "tasks": [{
+                            	        	   "title": "Create basic scaffold for assign-2",
+                            	        	   "status": "pending",
+                            	        	   "info": "Create a basic project structure for HU-Web",
+                            	        	   "deuDate": "27-06-2014", "createdOn": "26-06-2014", "closedOn": "27-06-2014",
+                            	        	   "assignedTo": "Chandan"
+                            	           	},
+                            	           	{
+                            	        	   "title": "Ensure responsive for assign-2",
+                            	        	   "status": "pending",
+                            	        	   "info": "Create css to ensure responsiveness of site",
+                            	        	   "deuDate": "27-06-2014", "createdOn": "26-06-2014", "closedOn": "27-06-2014",
+                            	        	   "assignedTo": "Ralf"
+                            	           	}]
                              }];
     
     for(var i=0; i < $scope.projectDetails.length; i++) {
     	if($routeParams.projectId == $scope.projectDetails[i].projectId) {
     		$scope.projectInfo = $scope.projectDetails[i];
-    		
     		console.log($scope.projectInfo);
-    		
     	}
     }
     
@@ -62,17 +89,34 @@ angular.module('myApp.controllers', [])
 
 
 var ModalInstanceCtrl = function ($scope, $modalInstance, items) {
-
 	  $scope.items = items;
 	  $scope.selected = {
 	    item: $scope.items[0]
 	  };
-
 	  $scope.ok = function () {
 	    $modalInstance.close($scope.selected.item);
 	  };
-
 	  $scope.cancel = function () {
 	    $modalInstance.dismiss('cancel');
-	  };
 	};
+};
+	
+var ButtonsCtrl = function ($scope) {
+	$scope.singleModel = 1;
+	$scope.radioModel = 'Middle';
+	$scope.checkModel = {
+			left: false,
+		    middle: true,
+		    right: false
+	};
+};
+
+function Test($scope) {
+	$scope.status = [{type:1, name: 'Caio'}, {type:2, name: 'Ary'}, {type:1, name: 'Camila'}, {type:3, name: 'Daniel'}];
+	$scope.myFunction = function() {
+		$scope.myFilter = function(val) {
+			return val.type != 2;
+		};
+		alert("done");
+	};
+};
